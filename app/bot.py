e5d9e7fd26bd912e92b889810872b9118445306b
nillnum <liaollle--@gmail.com>
2023-12-06 13:40:34 +0800

    根据新机器部署进行了一次bug 的修复，删除了部分多余的代码

--- app/bot.py
+++ app/bot.py
@@ -11,22 +11,6 @@ from informer import TGInformer
 """ 
 logging.getLogger().setLevel(logging.INFO)
 
-
-env_file = 'informer.env' if os.path.isfile('informer.env') else '../informer.env'
-logging.info(f'env_file: {env_file}')
-  
-dotenv_path = Path(env_file)
-load_dotenv(dotenv_path=dotenv_path)
-
-# 检查输入的变量是否足够
-try:
-    account_id = os.environ['TELEGRAM_ACCOUNT_ID']
-except:
-    raise Exception('informer.py <account_id> - account_id is a required param')
-
-if not account_id:
-    raise Exception('Account ID required')
-
 if __name__ == '__main__':
 
     informer = TGInformer(
